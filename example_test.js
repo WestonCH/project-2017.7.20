23d5f4a28b6d81862b71e5cfd926978cec962334
程浩 <weastonCH@sjtu.edu.cn>
2017-07-30 17:11:14 +0800

    no message

---
+++ example_test.js
@@ -0,0 +1,23 @@
+var exam1_symbol = "MD";
+var exam1_number = 701;
+var exam2_symbol = "AZ";
+var exam2_number = 504;
+var exam3_symbol = "MO";
+var exam3_number = 491;
+var exam4_symbol = "TX";
+var exam4_number = 541;
+var exam5_symbol = "NC";
+var exam5_number = 448;
+var exam6_symbol = "PA";
+var exam6_number = 411;
+var exam7_symbol = "OR";
+var exam7_number = 298;
+
+if (exam1_symbol == sampleData[exam1_symbol]) document.write("pass test1");
+if (exam2_symbol == sampleData[exam2_symbol]) document.write("pass test2");
+if (exam3_symbol == sampleData[exam3_symbol]) document.write("pass test3");
+if (exam4_symbol == sampleData[exam4_symbol]) document.write("pass test4");
+if (exam5_symbol == sampleData[exam5_symbol]) document.write("pass test5");
+if (exam6_symbol == sampleData[exam6_symbol]) document.write("pass test6");
+if (exam7_symbol == sampleData[exam7_symbol]) document.write("pass test7");
+
